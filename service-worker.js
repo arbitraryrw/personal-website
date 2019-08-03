c02312966d5755f5fdd6a8ae3f079291599b0e71
Nikola <nickcucakovic@gmail.com>
2019-08-03 22:33:17 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "arbitraryrw.github.io/arbitraryrw.github.io/precache-manifest.4bb7a788837bc72deb149f992abb4a26.js"
+  "arbitraryrw.github.io/personal-website/precache-manifest.1e1df5417eb481ed490748bd33251f79.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("arbitraryrw.github.io/arbitraryrw.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("arbitraryrw.github.io/personal-website/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
